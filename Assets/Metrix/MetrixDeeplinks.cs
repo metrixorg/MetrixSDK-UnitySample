a98e2c5cdeed1c8e32fe57e098839856d7b8529e
Milad Heydari <miladheydari2011@gmail.com>
2019-08-13 15:56:06 +0430

    use TROPHiT tool for deeplink android

---
+++ Assets/Metrix/MetrixDeeplinks.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+public class MetrixDeeplinks : MonoBehaviour {
+
+	#if UNITY_IOS
+	[DllImport("__Internal")]
+	private static extern void MetrixDeeplinks_init(string gameObject = null, string deeplinkMethod = null);
+	#endif
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_IOS
+		if (Application.platform == RuntimePlatform.IPhonePlayer) {
+			MetrixDeeplinks_init(gameObject.name);
+		}
+		#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void onDeeplink(string deeplink) {
+		Debug.Log("onDeeplink " + deeplink);
+	}
+
+
+}
